23e09c2f93904f7b125c1ef71861a92004bf17b9
Francois Germishuizen <fgermishuizen@gmail.com>
2013-09-09 10:19:44 +0200

    added Linux version of compression (untested)

--- AppMan/compression.h
+++ AppMan/compression.h
@@ -4,11 +4,9 @@
 #include <Build.h>
 
 /*
- * NOTE: This class is stll in beginner stages and still has some issues
- *
- * Current issues:   
+ *   Current issues:
  *      * Location of 7zip console application (7z)
- *      * Linux code
+ *      * Testing Linux code
  *
  */
 
@@ -27,11 +25,17 @@ public:
     /**
     * \fn compress(QStringList dirs, QString toDir, QString name)
     * @brief compress all files in <dirs> using 7zip and stores a 7z file with name <name> at location <toDir>
+    * @param dirs Contains a list of all directory and file paths that needs to be compressessed
+    * @param toDir The path to the newly compressed file
+    * @param name Specifies the name of the newly compressed file
     */
     void compress(QStringList dirs, QString toDir, QString name);
     /**
-    * \fn compress(QString fromDir, QString toDir, QString name)
+    * \fn decompress(QString fromDir, QString toDir, QString name)
     * @brief decompress a 7z file located at <fromDir> with name <name> using 7zip and extracts at location <toDir>
+    * @param fromDir The directory path to the compressed file that needs to be decompressed
+    * @param toDir The directory path to the newly decompressed file
+    * @param name Specifies the name of the newly decompressed file
     */
     void decompress(QString fromDir, QString toDir, QString name);
 };
