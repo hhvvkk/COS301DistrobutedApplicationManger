79c657f2213fdefe090682543c540551cd58dece
Henko van Koesveld <hvkoesveld@gmail.com>
2013-09-10 11:48:06 +0200

    added initial gui for simulations

--- AppMan/compression.cpp
+++ AppMan/compression.cpp
@@ -6,26 +6,50 @@ compression::compression() {
 }
 
 void compression::compress(QStringList dirs, QString toDir, QString name){
-    QString s("7z");
-    QStringList args;
-    args.append("a");
-    args.append(toDir + name + ".7z"); //make sure <toDir> contains a path that ends with <\> else this line has to be modified to:  args.append(toDir + "\\" + name + ".7z")
-    args.append(dirs);
-    QProcess* p = new QProcess();
-    p->start(s,args);    
+    #ifdef WIN32
+        QString s("7zip/Win32/7z");
+        QStringList args;
+        args.append("a");
+        args.append(toDir + name + ".7z"); //**
+        args.append(dirs);
+        QProcess* p = new QProcess();
+        p->start(s,args);
+    #else
+        QString s("7zip/Unix/7z");
+        QStringList args;
+        args.append("a");
+        args.append("-ttar");
+        args.append(toDir + name + ".tar"); //**
+        args.append(dirs);
+        QProcess* p = new QProcess();
+        p->start(s,args);
+    #endif
 }
 
 void compression::decompress(QString fromDir, QString toDir, QString name){
-    QString s("7z");
-    QStringList args;
-    args.append("x");
-    args.append(fromDir + name + ".7z"); //make sure <fromDir> contains a path that ends with <\> else this line has to be modified to:  args.append(fromDir + "\\" + name + ".7z")
-    args.append("-y");
-    args.append("-o" + toDir);
-    QProcess* p = new QProcess();
-    p->start(s,args);
+    #ifdef WIN32
+        QString s("7zip/Win32/7z");
+        QStringList args;
+        args.append("x");
+        args.append(fromDir + name + ".7z"); //**
+        args.append("-y");
+        args.append("-o" + toDir);
+        QProcess* p = new QProcess();
+        p->start(s,args);
+    #else
+        QString s("7zip/Unix/7z");
+        QStringList args;
+        args.append("x");
+        args.append(fromDir + name + ".tar"); //**
+        args.append("-y");
+        args.append("-o" + toDir);
+        QProcess* p = new QProcess();
+        p->start(s,args);
+    #endif
 }
 
+
+// ** make sure <fromDir> contains a path that ends with </> else this line has to be modified to:  args.append(fromDir + "/" + name + ".7z")
 
 /* SAMPLE USAGE:
  *
@@ -33,12 +57,12 @@ void compression::decompress(QString fromDir, QString toDir, QString name){
  *
  *  QStringList dirList;
  *
- *  dirList.append("somepath\\somefile1.txt");
- *  dirList.append("somepath2\\somefile2.txt");
- *  dirList.append("somepath3\\somedirectory");
+ *  dirList.append("somepath/somefile1.txt");
+ *  dirList.append("somepath2/somefile2.txt");
+ *  dirList.append("somepath3/somedirectory");
  *
- *  c->compress(dirList,"path\\ToDir\\","compressedFileName");
+ *  c->compress(dirList,"path/ToDir/","compressedFileName");
  *
- *  c->decompress("path\\fromDir\\","path\\new\\ToDir\\","decompressedDirName");
+ *  c->decompress("path/fromDir/","path/new/ToDir/","decompressedDirName");
  *
  **/
